79b60a108dc8212db83475adcaef0063011178e8
Ashot Krapetyan <arshaluys.karapetyan@hitachi-powergrids.com>
2020-11-29 21:58:33 +0100

    ICommand implementation

--- src/EasySoftware.MvvmMini.Samples.Contacts/Dialogs/MessageBox/MessageBoxViewModel.cs
+++ src/EasySoftware.MvvmMini.Samples.Contacts/Dialogs/MessageBox/MessageBoxViewModel.cs
@@ -6,44 +6,41 @@ using EasySoftware.MvvmMini.Core;
 
 namespace EasySoftware.MvvmMini.Samples.Contacts.Dialogs.MessageBox
 {
-	public class MessageBoxViewModel : DialogViewModelBase, IMessageBoxViewModel
-	{
-		public MessageBoxViewModel(IViewAdapter viewAdapter, string message, string title, MessageBoxButton buttons) : base(viewAdapter)
-		{
-			this.DialogResult = MessageBoxResult.Cancel;
-			this.Title = title;
-			this.Message = message;
-			this.Buttons = buttons;
-			this.SetResultCommand = new RelayCommand<object>(this.SetResult);
-		}
+   public class MessageBoxViewModel : DialogViewModelBase, IMessageBoxViewModel
+   {
+      public MessageBoxViewModel(IViewAdapter viewAdapter, string message, string title, MessageBoxButton buttons) : base(viewAdapter)
+      {
+         this.DialogResult = MessageBoxResult.Cancel;
+         this.Title = title;
+         this.Message = message;
+         this.Buttons = buttons;
+         this.SetResultCommand = new RelayCommand<MessageBoxResult>(this.SetResult);
+      }
 
-		public ICommand SetResultCommand { get; }
+      public ICommand SetResultCommand { get; }
 
-		private string _message;
-		public string Message
-		{
-			get => this._message;
-			set => SetProperty(ref this._message, value);
-		}
+      private string _message;
+      public string Message
+      {
+         get => this._message;
+         set => SetProperty(ref this._message, value);
+      }
 
-		private MessageBoxButton _buttons;
-		public MessageBoxButton Buttons
-		{
-			get => this._buttons;
-			set => SetProperty(ref this._buttons, value);
-		}
+      private MessageBoxButton _buttons;
+      public MessageBoxButton Buttons
+      {
+         get => this._buttons;
+         set => SetProperty(ref this._buttons, value);
+      }
 
-		public MessageBoxResult DialogResult { get; private set; }
+      public MessageBoxResult DialogResult { get; private set; }
 
-		private Task SetResult(object arg)
-		{
-			if (arg is MessageBoxResult result)
-			{
-				this.DialogResult = result;
-				this.CloseCommand.Execute(null);
-			}
+      private Task SetResult(MessageBoxResult result)
+      {
+         this.DialogResult = result;
+         this.CloseCommand.Execute(null);
 
-			return Task.CompletedTask;
-		}
-	}
+         return Task.CompletedTask;
+      }
+   }
 }
--- src/EasySoftware.MvvmMini/RelayCommand.cs
+++ src/EasySoftware.MvvmMini/RelayCommand.cs
@@ -4,79 +4,109 @@ using System.Windows.Input;
 
 namespace EasySoftware.MvvmMini
 {
-	public class RelayCommand<T> : ICommand where T : class
-	{
+   public class RelayCommand<T> : ICommand
+   {
+      private readonly Func<T, Task> _execute;
+      private readonly Func<T, bool> _canExecute;
+      private bool _isLoad;
 
-		private readonly Func<T, Task> _execute;
-		private readonly Func<bool> _canExecute;
-		private bool _isLoad = false;
+      public RelayCommand(Func<T, Task> execute, Func<T, bool> canExecute = null)
+      {
+         _execute = execute;
+         _canExecute = canExecute;
+      }
 
-		public RelayCommand(Func<T, Task> execute, Func<bool> canExecute = null)
-		{
-			_execute = execute;
-			_canExecute = canExecute;
-		}
+      public bool CanExecute(object parameter)
+      {
+         if (this._isLoad)
+            return false;
 
-		public bool CanExecute(object parameter)
-		{
-			if (this._isLoad)
-				return false;
+         if (_canExecute != null)
+            return _canExecute((T)parameter);
 
-			if (_canExecute != null)
-				return _canExecute();
+         return true;
+      }
 
-			return true;
-		}
+      public async void Execute(object parameter)
+      {
+         this._isLoad = true;
+         await _execute((T)parameter);
+         this._isLoad = false;
+         this.RaiseCanExecuteChanged();
+      }
 
-		public async void Execute(object parameter)
-		{
-			this._isLoad = true;
-			await _execute(parameter as T);
-			this._isLoad = false;
-			this.RaiseCanExecuteChanged();
-		}
+      public event EventHandler CanExecuteChanged
+      {
+         add
+         {
+            CommandManager.RequerySuggested += value;
+            LocalCanExecuteChanged += value;
+         }
+         remove
+         {
+            CommandManager.RequerySuggested -= value;
+            LocalCanExecuteChanged -= value;
+         }
+      }
 
-		public Task ExecuteAsync(object parameter)
-		{
-			return Task.FromResult(this._execute);
-		}
+      public event EventHandler LocalCanExecuteChanged;
 
-		public event EventHandler CanExecuteChanged
-		{
-			add
-			{
-				CommandManager.RequerySuggested += value;
-				LocalCanExecuteChanged += value;
-			}
-			remove
-			{
-				CommandManager.RequerySuggested -= value;
-				LocalCanExecuteChanged -= value;
-			}
-		}
+      public void RaiseCanExecuteChanged()
+      {
+         LocalCanExecuteChanged?.Invoke(this, new EventArgs());
+      }
+   }
 
-		public event EventHandler LocalCanExecuteChanged;
+   public class RelayCommand : ICommand
+   {
+      private readonly Func<Task> _execute;
+      private readonly Func<bool> _canExecute;
+      private bool _isLoad;
 
-		public void RaiseCanExecuteChanged()
-		{
-			LocalCanExecuteChanged?.Invoke(this, new EventArgs());
-		}
+      public RelayCommand(Func<Task> execute, Func<bool> canExecute = null)
+      {
+         _execute = execute;
+         _canExecute = canExecute;
+      }
 
-	}
+      public bool CanExecute(object parameter)
+      {
+         if (this._isLoad)
+            return false;
 
-	public class RelayCommand : RelayCommand<object>
-	{
-		private string login;
-		private Func<bool> canLogin;
-		private ICommand save;
-		private Func<bool> canSave;
+         if (_canExecute != null)
+            return _canExecute();
 
-		public RelayCommand(Func<Task> execute, Func<bool> canExecute = null)
-			: base(o => execute(), canExecute)
-		{
+         return true;
+      }
 
-		}
+      public async void Execute(object parameter)
+      {
+         this._isLoad = true;
+         await _execute();
+         this._isLoad = false;
+         this.RaiseCanExecuteChanged();
+      }
 
-	}
+      public event EventHandler CanExecuteChanged
+      {
+         add
+         {
+            CommandManager.RequerySuggested += value;
+            LocalCanExecuteChanged += value;
+         }
+         remove
+         {
+            CommandManager.RequerySuggested -= value;
+            LocalCanExecuteChanged -= value;
+         }
+      }
 
+      public event EventHandler LocalCanExecuteChanged;
+
+      public void RaiseCanExecuteChanged()
+      {
+         LocalCanExecuteChanged?.Invoke(this, new EventArgs());
+      }
+   }
 }
