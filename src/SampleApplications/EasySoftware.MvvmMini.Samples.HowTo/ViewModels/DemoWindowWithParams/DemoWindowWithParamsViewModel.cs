553e3c77ed2f0ad90299c47ccd75ab377a07fd62
shota <karapetyanshota@gmail.com>
2021-01-26 01:26:57 +0100

    sample applications moved to separate directory

---
+++ src/SampleApplications/EasySoftware.MvvmMini.Samples.HowTo/ViewModels/DemoWindowWithParams/DemoWindowWithParamsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using EasySoftware.MvvmMini.Core;
+
+namespace EasySoftware.MvvmMini.Samples.HowTo.ViewModels.DemoWindowWithParams
+{
+	public interface IDemoWindowWithParamsViewModel: IWindowViewModel { }
+
+	public class DemoWindowWithParamsViewModel : WindowViewModelBase, IDemoWindowWithParamsViewModel
+	{
+		public DemoWindowWithParamsViewModel(IViewAdapter viewAdapter, DateTime dateTime) : base(viewAdapter)
+		{
+			this.Title = "Demo window with params";
+			this.WelcomeText = $"Parameter passed to view model: {dateTime.ToString("HH:mm:ss")}";
+		}
+
+		private string _welcomeText;
+		public string WelcomeText
+		{
+			get => this._welcomeText;
+			set => SetProperty(ref this._welcomeText, value);
+		}
+	}
+}
