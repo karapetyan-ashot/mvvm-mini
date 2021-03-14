5a0a72c1b141c3de93cc758ad321cba846acadac
Ashot Krapetyan <arshaluys.karapetyan@hitachi-powergrids.com>
2021-03-14 18:14:21 +0100

    RelayCommand improvements

--- src/EasySoftware.MvvmMini/RelayCommand.cs
+++ src/EasySoftware.MvvmMini/RelayCommand.cs
@@ -6,105 +6,117 @@ using EasySoftware.MvvmMini.Core;
 
 namespace EasySoftware.MvvmMini
 {
-	public abstract class RelayCommandBase : IRelayCommand
-	{
-		private event EventHandler _canExecuteChanged;
-		protected bool _useCommandManager = true;
+   public abstract class RelayCommandBase : IRelayCommand
+   {
+      protected bool _useCommandManager = true;
+      protected bool _isRunning;
 
-		public event EventHandler CanExecuteChanged
-		{
-			add
-			{
-				if (this._useCommandManager)
-					CommandManager.RequerySuggested += value;
-				else
-					this._canExecuteChanged += value;
-			}
-			remove
-			{
-				if (this._useCommandManager)
-					CommandManager.RequerySuggested -= value;
-				else
-					this._canExecuteChanged -= value;
-			}
-		}
+      private event EventHandler _canExecuteChanged;
+      public event EventHandler CanExecuteChanged
+      {
+         add
+         {
+            if (this._useCommandManager)
+               CommandManager.RequerySuggested += value;
+            else
+               this._canExecuteChanged += value;
+         }
+         remove
+         {
+            if (this._useCommandManager)
+               CommandManager.RequerySuggested -= value;
+            else
+               this._canExecuteChanged -= value;
+         }
+      }
 
-		public abstract void Execute(object parameter);
+      public abstract void Execute(object parameter);
 
-		public abstract bool CanExecute(object parameter);
+      public abstract bool CanExecute(object parameter);
 
-		public void RaiseCanExecuteChanged()
-		{
-			if (this._useCommandManager)
-				CommandManager.InvalidateRequerySuggested();
-			else
-				this._canExecuteChanged?.Invoke(this, EventArgs.Empty);
-		}
-	}
+      public void RaiseCanExecuteChanged()
+      {
+         if (this._useCommandManager)
+            CommandManager.InvalidateRequerySuggested();
+         else
+            this._canExecuteChanged?.Invoke(this, EventArgs.Empty);
+      }
+   }
 
-	public class RelayCommand<T> : RelayCommandBase
-	{
-		private readonly Func<T, Task> _execute;
-		private readonly Func<T, bool> _canExecute;
-		private bool _isRunning;
+   public class RelayCommand<T> : RelayCommandBase
+   {
+      private readonly Func<T, Task> _execute;
+      private readonly Func<T, bool> _canExecute;
 
-		public RelayCommand(Func<T, Task> execute, Func<T, bool> canExecute = null, bool useCommandManager = true)
-		{
-			this._execute = execute ?? throw new ArgumentNullException(nameof(execute));
-			this._canExecute = canExecute;
-			this._useCommandManager = useCommandManager;
-		}
+      public RelayCommand(Func<T, Task> execute)
+         : this(execute, null, false) { }
 
-		public override async void Execute(object parameter)
-		{
-			this._isRunning = true;
-			await _execute((T)parameter);
-			this._isRunning = false;
-			this.RaiseCanExecuteChanged();
-		}
+      public RelayCommand(Func<T, Task> execute, Func<T, bool> canExecute)
+         : this(execute, canExecute, true) { }
 
-		public override bool CanExecute(object parameter)
-		{
-			if (this._isRunning)
-				return false;
 
-			if (_canExecute == null)
-				return true;
+      public RelayCommand(Func<T, Task> execute, Func<T, bool> canExecute, bool useCommandManager)
+      {
+         this._execute = execute ?? throw new ArgumentNullException(nameof(execute));
+         this._canExecute = canExecute;
+         this._useCommandManager = useCommandManager;
+      }
 
-			return _canExecute((T)parameter);
-		}
-	}
+      public override async void Execute(object parameter)
+      {
+         this._isRunning = true;
+         await _execute((T)parameter);
+         this._isRunning = false;
+         this.RaiseCanExecuteChanged();
+      }
 
-	public class RelayCommand : RelayCommandBase
-	{
-		private readonly Func<Task> _execute;
-		private readonly Func<bool> _canExecute;
-		private bool _isRunning;
+      public override bool CanExecute(object parameter)
+      {
+         if (this._isRunning)
+            return false;
 
-		public RelayCommand(Func<Task> execute, Func<bool> canExecute = null, bool useCommandManager = true)
-		{
-			this._execute = execute ?? throw new ArgumentNullException(nameof(execute));
-			this._canExecute = canExecute;
-			this._useCommandManager = useCommandManager;
-		}
+         if (_canExecute == null)
+            return true;
 
-		public override async void Execute(object parameter)
-		{
-			this._isRunning = true;
-			await _execute();
-			this._isRunning = false;
-			this.RaiseCanExecuteChanged();
-		}
+         return _canExecute((T)parameter);
+      }
+   }
 
-		public override bool CanExecute(object parameter)
-		{
-			if (this._isRunning)
-				return false;
+   public class RelayCommand : RelayCommandBase
+   {
+      private readonly Func<Task> _execute;
+      private readonly Func<bool> _canExecute;
 
-			if (_canExecute == null)
-				return true;
+      public RelayCommand(Func<Task> execute)
+         : this(execute, null, false) { }
 
-			return _canExecute();
-		}
-	}
+      public RelayCommand(Func<Task> execute, Func<bool> canExecute)
+         : this(execute, canExecute, true) { }
+
+      public RelayCommand(Func<Task> execute, Func<bool> canExecute, bool useCommandManager)
+      {
+         this._execute = execute ?? throw new ArgumentNullException(nameof(execute));
+         this._canExecute = canExecute;
+         this._useCommandManager = useCommandManager;
+      }
+
+      public override async void Execute(object parameter)
+      {
+         this._isRunning = true;
+         await _execute();
+         this._isRunning = false;
+         this.RaiseCanExecuteChanged();
+      }
+
+      public override bool CanExecute(object parameter)
+      {
+         if (this._isRunning)
+            return false;
+
+         if (_canExecute == null)
+            return true;
+
+         return _canExecute();
+      }
+   }
 }
